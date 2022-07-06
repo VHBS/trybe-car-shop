03f50fb094769f22d06aa8ecbb67f54af704d21f
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-06 20:36:56 -0300

    feat: car service

---
+++ src/services/CarService.ts
@@ -0,0 +1,34 @@
+import { Car, CarSchema } from '../interfaces/CarInterface';
+import Service, { ServiceError } from '.';
+import CarModel from '../models/CarModel';
+
+export default class CarService extends Service<Car> {
+  constructor(_model = new CarModel()) {
+    super(_model);
+  }
+
+  create = async (entity: Car): Promise<Car | null | ServiceError> => {
+    const parsed = CarSchema.safeParse(entity);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this._model.create(entity);
+  };
+
+  read = async (): Promise<Car[]> => this._model.read();
+
+  readOne = async (id: string): Promise<Car | null> =>
+    this._model.readOne(id);
+
+  update = async (id: string, entity: Car): 
+  Promise<Car | null | ServiceError> => {
+    const parsed = CarSchema.safeParse(entity);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this._model.update(id, entity);
+  };
+
+  delete = async (id: string): Promise<Car | null> =>
+    this._model.delete(id);
+}
