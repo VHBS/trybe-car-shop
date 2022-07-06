12065088fb35b3082318a43a04bcbc72616693da
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-06 19:53:15 -0300

    feat: car schema

---
+++ src/models/schemas/CarSchema.ts
@@ -0,0 +1,34 @@
+import { Document, Schema } from 'mongoose';
+import { Car } from '../../interfaces/CarInterface';
+
+export interface CarDocument extends Car, Document {}
+
+const carSchema = new Schema<CarDocument>({
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  status: Boolean,
+  buyValue: {
+    type: Number,
+    required: true,
+  },
+  doorsQty: {
+    type: Number,
+    required: true,
+  },
+  seatsQty: {
+    type: Number,
+    required: true,
+  },
+});
+
+export default carSchema;
