4fada44b01d6eefc9a5b71cffc59b9440ee6e5b0
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-07 01:53:29 -0300

    test: car model create

---
+++ src/tests/unit/models/carModel.test.ts
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+import * as sinon from 'sinon';
+import chai from 'chai';
+import CarModel from '../../../models/CarModel';
+const { expect } = chai;
+
+import { Car } from '../../../interfaces/CarInterface';
+
+describe('Car Model', () => {
+
+  describe('Create car', () => {
+    const newCar = {
+      model: "Ferrari Maranello",
+      year: 1963,
+      color: "red",
+      buyValue: 3500000,
+      doorsQty: 2,
+      seatsQty: 2,
+      __v: 0
+    } as Car;
+
+    const mockCreatedCar = {
+      model: "Ferrari Maranello",
+      year: 1963,
+      color: "red",
+      buyValue: 3500000,
+      doorsQty: 2,
+      seatsQty: 2,
+      _id: "62c62ed6cfa81704b0a5293e",
+      __v: 0
+    };
+    
+    before(async () => {
+      sinon.stub(mongoose.Model, 'create').resolves(mockCreatedCar);
+    });
+    
+    after(() => {
+      sinon.restore();
+    })
+    
+    it('Success', async () => {
+      const carModel = new CarModel();
+      const createdCar = await carModel.create(newCar)
+
+      expect(createdCar).deep.equal(mockCreatedCar)
+    });
+  })
+});
