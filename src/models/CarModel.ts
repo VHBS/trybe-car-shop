363842d67dac450fb852f0aadaf4b0839272a9c2
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-07 16:34:38 -0300

    refacor: car model

--- src/models/CarModel.ts
+++ src/models/CarModel.ts
@@ -1,9 +1,8 @@
 import { Model } from 'mongoose';
 import MongoModel from '.';
 import { CarDocument } from './schemas/CarSchema';
-import { Car } from '../interfaces/CarInterface';
 
-export default class CarModel extends MongoModel<Car> {
+export default class CarModel extends MongoModel<CarDocument> {
   constructor(model: Model<CarDocument>) {
     super(model);
     this._model = model;
