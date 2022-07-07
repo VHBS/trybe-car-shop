0a729dbb31d846fcb37731055bf8d49bd5e52a7e
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-06 22:20:36 -0300

    feat: add car router create

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-// import CustomRouter from './routes/Router';
 import App from './app';
+import CarController from './controllers/CarController';
+import { Car } from './interfaces/CarInterface';
+import CustomRouter from './routes';
 
 // import exampleController from './controllers/controller-example';
-
 // import { example } from './interfaces/ExampleInterface';
+// exampleRouter.addRoute(exampleController);
+// server.addRouter(exampleRouter.router);
 
 const server = new App();
 
-// const exampleController = new exampleController();
+const carController = new CarController();
 
-// const exampleRouter = new CustomRouter<Car>();
-// exampleRouter.addRoute(exampleController);
-
-// server.addRouter(exampleRouter.router);
+const carRouter = new CustomRouter<Car>();
+carRouter.addRoute(carController);
+server.addRouter(carRouter.router);
 
 export default server;
